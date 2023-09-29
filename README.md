b23cd20d139bf47c1a408f1b0ebe7d68eb010089
Hansae Ju <sparky@hanyang.ac.kr>
2023-09-29 13:40:25 +0900

    writing readme

--- README.md
+++ README.md
@@ -3,13 +3,7 @@
 This is the replication package for the paper "Evaluating the Effectiveness of Actionable Guidance for Defect Prediction from Developers' Perspective"
 
 ## ToC
-- [DeFlip Replication Package](#deflip-replication-package)
-  - [ToC](#toc)
-  - [DeFlip](#deflip)
-    - [Usage](#usage)
-  - [Installation](#installation)
-  - [Replication Researh Questions](#replication-researh-questions)
-  - [Appendix](#appendix)
+
 
 ## DeFlip
 
@@ -39,6 +33,51 @@ poetry install -E all_models
 
 ## Replication Researh Questions
 
+### Preprocess
+#### Prerequisites
+```
+R 
+AutoSpearman
+rpy2
+```
+
+```bash
+python preprocess.py
+```
+Our preprocessing steps include:
+- Generate (k-1 , k) release (train, test) dataset from original dataset (Yatish et al.)
+- Feature selection using AutoSpearman
+
+or you can just use the preprocessed dataset in `Dataset/release_dataset`
+
+### Run Explainers to generate proposed changes
+for LIME-HPO and TimeLIME: 2 steps
+```bash
+python run_explainers.py --explainer "LIMEHPO" --project "activemq@2"
+python plan_explanations.py --explainer "LIMEHPO" --project "activemq@2" # --only_minimum for MPC
+```
+The first step generates the explanation-based proposed changes, and the second step generates the all possible changes or the minimum changes.
+
+for SQAPlanner: 3 steps
+```bash
+python run_explainers.py --explainer "SQAPlanner" --project "activemq@2"
+cd Explainers/SQAPlanner
+python mining_sqa_rules.py --project "activemq@2" --search_strategy confidence # Requires BigML API key
+python plan_explanations.py --explainer "SQAPlanner" --search_strategy confidence --project "activemq@2" # --only_minimum for MPC
+```
+The first step generates random neighbor instances using training data, and the second step generates the association rules using BigML API. The third step generates the explanation-based proposed changes.
+
+### Flip checking
+```bash
+python flip_exp.py --explainer "TimeLIME" # RQ1 (a) Flip check with possible flipping proposed changes 
+python flip_exp.py --explainer "TimeLIME" --only_minimum # RQ1 (b) Flip check with minimum proposed changes
+```
+The flipped instances are stored in `flipped_instances` folder and flip rates are stored in `flip_rates` folder.
+
+### Research Questions
+```bash
+python research_questions.py 
+```
 
 ## Appendix
 
--- preprocess.py
+++ preprocess.py
@@ -5,10 +5,9 @@ from rpy2.robjects.packages import importr
 from rpy2.robjects import pandas2ri, StrVector
 import pandas as pd
 
+
 def map_indexes_to_int(train_df, test_df):
-    all_files = (
-        train_df.index.append(test_df.index).unique()
-    )
+    all_files = train_df.index.append(test_df.index).unique()
     file_to_int = {file: i for i, file in enumerate(all_files)}
     int_to_file = {i: file for file, i in file_to_int.items()}  # 역매핑
 
@@ -17,12 +16,15 @@ def map_indexes_to_int(train_df, test_df):
 
     return train_df, test_df, int_to_file  # 역매핑도 반환
 
+
 def remove_variables(df, vars_to_remove):
     df.drop(vars_to_remove, axis=1, inplace=True, errors="ignore")
+
 
 def get_df(project: str, release: str, path_data: str = "project_dataset"):
     df = pd.read_csv(f"{path_data}/{project}/{release}", index_col=0)
     return df
+
 
 def preprocess(project, releases: list[str]):
     dataset_trn = get_df(project, releases[0])
@@ -35,8 +37,12 @@ def preprocess(project, releases: list[str]):
     dataset_tst = dataset_tst[~duplicated_index_tst]
 
     print(f"Project: {project}")
-    print(f"Release: {releases[0]} total: {len(dataset_trn)} bug: {len(dataset_trn[dataset_trn['RealBug'] == 1])}")
-    print(f"Release: {releases[1]} total: {len(dataset_tst)} bug: {len(dataset_tst[dataset_tst['RealBug'] == 1])}")
+    print(
+        f"Release: {releases[0]} total: {len(dataset_trn)} bug: {len(dataset_trn[dataset_trn['RealBug'] == 1])}"
+    )
+    print(
+        f"Release: {releases[1]} total: {len(dataset_tst)} bug: {len(dataset_tst[dataset_tst['RealBug'] == 1])}"
+    )
 
     # dataset_tst에서 dataset_trn에 존재하는 동일한 인덱스와 모든 칼럼의 값이 동일한 행을 제거
     dataset_tst = dataset_tst.drop(
@@ -46,7 +52,6 @@ def preprocess(project, releases: list[str]):
         errors="ignore",
     )
 
-
     vars_to_remove = ["HeuBug", "RealBugCount", "HeuBugCount"]
     remove_variables(dataset_trn, vars_to_remove)
     remove_variables(dataset_tst, vars_to_remove)
@@ -54,12 +59,10 @@ def preprocess(project, releases: list[str]):
     dataset_trn = dataset_trn.rename(columns={"RealBug": "target"})
     dataset_tst = dataset_tst.rename(columns={"RealBug": "target"})
 
-    dataset_trn['target'] = dataset_trn['target'].astype(bool)
-    dataset_tst['target'] = dataset_tst['target'].astype(bool)
+    dataset_trn["target"] = dataset_trn["target"].astype(bool)
+    dataset_tst["target"] = dataset_tst["target"].astype(bool)
 
-    dataset_trn, dataset_tst, mapping = map_indexes_to_int(
-        dataset_trn, dataset_tst
-    )
+    dataset_trn, dataset_tst, mapping = map_indexes_to_int(dataset_trn, dataset_tst)
     Rnalytica = importr("Rnalytica")
     features_names = dataset_trn.columns.tolist()[:-1]
     X_train = dataset_trn.loc[:, features_names].copy()
@@ -71,7 +74,7 @@ def preprocess(project, releases: list[str]):
     selected_features = list(selected_features) + ["target"]
     train = dataset_trn.loc[:, selected_features]
     test = dataset_tst.loc[:, selected_features]
-    
+
     return train, test, mapping
 
 
@@ -98,6 +101,7 @@ def organize_original_dataset():
     path_truncate(Path("./project_dataset/lucene"))
     path_truncate(Path("./project_dataset/wicket"))
 
+
 def path_truncate(project, base="src/"):
     print(f"Project: {project.name}")
     for path in project.glob("*.csv"):
@@ -105,13 +109,15 @@ def path_truncate(project, base="src/"):
         df.index = df.index.map(lambda x: split_path(base, x))
         df.to_csv(path)
 
+
 def split_path(base, path):
     if base in path:
         _, *tail = path.split(base)
         return base + base.join(tail)
     else:
         return path
-    
+
+
 def prepare_release_dataset():
     projects = all_dataset()
     for project, releases in projects.items():
@@ -121,10 +127,11 @@ def prepare_release_dataset():
             Path(save_folder).mkdir(parents=True, exist_ok=True)
             dataset_trn.to_csv(save_folder + "/train.csv", index=True, header=True)
             dataset_tst.to_csv(save_folder + "/test.csv", index=True, header=True)
-            
+
             # Save mapping as csv
             mapping_df = pd.DataFrame.from_dict(mapping, orient="index")
             mapping_df.to_csv(save_folder + "/mapping.csv", index=True, header=False)
+
 
 if __name__ == "__main__":
     organize_original_dataset()
